e5dd1d64b4c72eab04b91f99e45049762c21f89e
Timur <95653755+OgGospod1n@users.noreply.github.com>
2022-10-03 21:51:12 +0300

    Add files via upload

---
+++ Rootss.java
@@ -0,0 +1,12 @@
+public class Rootss {
+    public double x1;
+    public double x2;
+    public void print(){
+        if(x1 != x2 && x2!=0) {
+            System.out.println(x1);
+            System.out.println(x2);
+        }
+        else System.out.println(x1);
+    }
+}
+
