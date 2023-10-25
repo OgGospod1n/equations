082ff6922c2db6553a86e931d3bd593203f32ddf
Timur <95653755+OgGospod1n@users.noreply.github.com>
2023-10-26 02:46:09 +0300

    Add files via upload

---
+++ livewire-components.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'breadcrumb' => 'App\\Http\\Livewire\\Breadcrumb',
+  'dashboard' => 'App\\Http\\Livewire\\Dashboard',
+  'document.create' => 'App\\Http\\Livewire\\Document\\Create',
+  'document.edit' => 'App\\Http\\Livewire\\Document\\Edit',
+  'document.share' => 'App\\Http\\Livewire\\Document\\Share',
+  'document.show' => 'App\\Http\\Livewire\\Document\\Show',
+  'search' => 'App\\Http\\Livewire\\Search',
+);
